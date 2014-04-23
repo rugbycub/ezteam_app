b897d7d7767f1dfa3d5d13c4b4ae25acf5482e03
Charlie Nuttall <CENuttall@mac.com>
2014-04-23 09:37:09 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EzteamApp::Application.config.secret_key_base = 'e51ef1d1fd0efc7c7faf99c42a76c134d9c7329cce2b75aebddaf112dc0c01c96661081bff3c5920510ffbf48284181a1dbe6f52f056a2dc0ab87c69a636e527'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EzteamApp::Application.config.session_store :cookie_store, key: '_ezteam_app_session'
